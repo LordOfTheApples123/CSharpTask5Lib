9b13cd8c0e32e1ea4e45e838901b970201aa5569
LordOfTheApples123 <rus.khalilov1980@mail.ru>
2023-06-01 16:54:50 +0300

    init

---
+++ ClassLibrary1/ModelA.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1;
+
+public class ModelA: SiemensMobile
+{
+    public ModelA(string company, int prop1, double prop2, string a1, int a2) : base(company, prop1, prop2)
+    {
+        A1 = a1;
+        A2 = a2;
+    }
+
+    private string A1
+    {
+        get;
+        set;
+    }
+
+    private int A2
+    {
+        get;
+        set;
+    }
+
+    public string Amethod1()
+    {
+        return A2.ToString();
+    }
+
+    public int AMethod2(int x1, int x2)
+    {
+        return x1 + x2;
+    }
+}
---
+++ ClassLibrary1/ModelB.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp1;
+
+namespace ClassLibrary1;
+
+public class ModelB: SiemensMobile
+{
+    public ModelB(string company, int prop1, double prop2, string b2, int b1) : base(company, prop1, prop2)
+    {
+        B2 = b2;
+        B1 = b1;
+    }
+
+    private string B2
+    {
+        get;
+        set;
+    }
+
+    private int B1
+    {
+        get;
+        set;
+    }
+
+    public int B1Method(int x)
+    {
+        return B1 + x;
+    }
+
+    public string B2Method(string x)
+    {
+        return B2 + x;
+    }
+}
